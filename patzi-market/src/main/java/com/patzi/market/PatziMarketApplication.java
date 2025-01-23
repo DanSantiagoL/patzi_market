6516ead952cee88d5b3aeacf98bb3e534a56be17
Daniel López <101286296+DanSantiagoL@users.noreply.github.com>
2025-01-23 14:48:31 -0500

    Add files via upload

---
+++ patzi-market/src/main/java/com/patzi/market/PatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.patzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatziMarketApplication.class, args);
+	}
+
+}
